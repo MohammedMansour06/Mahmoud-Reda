9f192bc6b4c2910222963b951c4fc37bf95c6c18
MohammedMansour06 <mohamedmansor35.m@gmail.com>
2025-06-27 10:50:33 +0300

    Add files via upload

---
+++ String Library Project C10.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include"clsString.h"
+
+using namespace std; 
+
+
+int main()
+{
+  
+	clsString String1("Mohammed Abu-Hadhoud");
+	 
+	cout << String1.CountVowels() << endl;
+	
+
+	
+	 
+
+
+
+
+	system("pause>0"); 
+	return 0; 
+
+
+}
